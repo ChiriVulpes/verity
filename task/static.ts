ea42ecab62e7fda4d73f83c2ee089fd2e3a9e8f8
Chiri Vulpes <chiri@chiri.works>
2025-09-02 02:20:24 +1200

    Fix html being outputted with wrong origin

--- task/static.ts
+++ task/static.ts
@@ -10,5 +10,5 @@ export default Task('static', async task => {
 	await fs.rm('out/static', { recursive: true, force: true })
 	await fs.cp('static', 'out/static', { recursive: true, force: true })
 	const index = await fs.readFile('index.html', 'utf8')
-	await fs.writeFile('out/index.html', index.replaceAll('./', `https://localhost:${Env.PORT}/`))
+	await fs.writeFile('out/index.html', index.replaceAll('./', `${(Env.ORIGIN ?? `https://localhost:${Env.PORT}`)}/`))
 })
